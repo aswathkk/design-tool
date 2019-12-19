7baeea8bf6fe0340bf020267b8f73431a2188f72
Aswath K <aswath.sana@gmail.com>
2019-12-19 22:52:37 +0530

    Add close button for image selection

--- src/components/ImageBtn/index.js
+++ src/components/ImageBtn/index.js
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from 'react';
 import { fabric } from 'fabric';
 import ImageOutlinedIcon from '@material-ui/icons/ImageOutlined';
 import CloudUploadOutlinedIcon from '@material-ui/icons/CloudUploadOutlined';
+import CancelIcon from '@material-ui/icons/Cancel';
 import { CanvasContext } from '../../utils/useCanvas';
 
 import style from './style.module.css';
@@ -38,7 +39,16 @@ const ImageUploadBtn = props => {
   );
 }
 
-const ImageSelector = () => {
+const ImageDisplay = props => {
+  return (
+    <div style={{ position: 'relative' }}>
+      <CancelIcon onClick={props.onRemoveImage} style={{ position: 'absolute', top: '10px', right: '2px' }} />
+      <img onClick={props.onClick} src={props.src} className={style.image} alt={props.alt} />
+    </div>
+  )
+}
+
+const ImageSelector = props => {
   const { canvas } = useContext(CanvasContext);
   const [images, setImages] = useState(getImagesFromLocalStorage());
 
@@ -56,19 +66,26 @@ const ImageSelector = () => {
     });
   }
 
+  const removeImage = id => {
+    localStorage.setItem('images', JSON.stringify(images.slice(0, id).concat(images.slice(id + 1))));
+    reloadImages();
+  }
+
   return (
     <div className={style.imageSelectorRoot}>
-      <h1>Select Image</h1>
+      <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
+        <CancelIcon onClick={props.onClose} />
+      </div>
       <div className={style.row}>
         <div className={style.column}>
           <ImageUploadBtn onFileUpload={reloadImages} />
           {images.map((image, i) =>
-            (i % 2 === 0) && <img onClick={() => onImgClick(image)} className={style.image} key={i} src={image} alt={`upload-$[i]`} />
+            (i % 2 === 0) && <ImageDisplay onRemoveImage={() => removeImage(i)} onClick={() => onImgClick(image)} key={i} src={image} alt={`upload-$[i]`} />
           )}
         </div>
         <div className={style.column}>
           {images.map((image, i) =>
-            (i % 2 === 1) && <img onClick={() => onImgClick(image)} className={style.image} key={i} src={image} alt={`upload-$[i]`} />
+            (i % 2 === 1) && <ImageDisplay onRemoveImage={() => removeImage(i)} onClick={() => onImgClick(image)} key={i} src={image} alt={`upload-$[i]`} />
           )}
         </div>
       </div>
@@ -92,7 +109,7 @@ const ImageBtn = (props) => {
         <ImageOutlinedIcon style={{color: isSelected ? '#000' : '#888'}} /> 
       </div>
       {isSelected &&
-      <ImageSelector />
+        <ImageSelector onClose={() => setIsSelected(false)} />
       }
     </div>
   );
