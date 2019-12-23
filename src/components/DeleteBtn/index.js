1cfc98aaf2ab5210293e8de0f579927145c5e4ca
Aswath K <aswath.sana@gmail.com>
2019-12-23 23:41:32 +0530

    Support delete key for deleting objects

--- src/components/DeleteBtn/index.js
+++ src/components/DeleteBtn/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { CanvasContext } from '../../utils/useCanvas';
 import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
 
@@ -10,6 +10,18 @@ const DeleteBtn = (props) => {
       canvas.remove(object)
     });
   }
+
+  const onKeyDown = e => {
+    if (e.key === 'Delete')
+      remove();
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', onKeyDown);
+    return () => {
+      document.removeEventListener('keydown', onKeyDown);
+    }
+  })
 
   return (
     <div style={{ padding: '7px', cursor: 'pointer' }} onClick={remove}>
