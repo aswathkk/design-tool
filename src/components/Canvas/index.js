17a972728c411f3ac17b1c39b90fb10ae3eb56ea
Aswath K <aswath.sana@gmail.com>
2019-12-30 21:37:16 +0530

    Add download button

--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useContext, useRef } from 'react';
 import { fabric } from 'fabric';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import GetAppIcon from '@material-ui/icons/GetApp';
 
 import toggleLock from './utils/toggleLock';
 import styles from './canvas.module.css';
@@ -7,7 +11,18 @@ import { CanvasContext } from '../../utils/useCanvas';
 
 toggleLock(fabric);
 
+const useStyles = makeStyles(theme => ({
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
 const Canvas = (props) => {
+  const classes = useStyles();
   const myCanvas = useRef();
   const { setCanvas } = useContext(CanvasContext);
 
@@ -17,16 +32,29 @@ const Canvas = (props) => {
     window.canvas = canvas;
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  
+  const downloadImage = () => {
+    let link = document.createElement('a');
+    link.href = window.canvas.toDataURL('png');
+    link.download = 'download.png';
+    link.click();
+  }
 
   return(
     <div className={styles.root}>
-      <div className={styles.canvas}>
-        <canvas
-          width={props.width}
-          height={props.height}
-          ref={myCanvas}
-          id="my-canvas"
-        />
+      <div>
+        <div className={styles.canvas}>
+          <canvas
+            width={props.width}
+            height={props.height}
+            ref={myCanvas}
+            id="my-canvas"
+          />
+        </div>
+        <div className={classes.buttonContainer}>
+          <IconButton onClick={downloadImage} variant="contained"><GetAppIcon /></IconButton>
+          <Button variant="contained" color="primary">Save</Button>
+        </div>
       </div>
     </div>
   );
